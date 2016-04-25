e4f7c823f80afa56e00e107209c168884ea9937c
Patrick Freeman <trick05@bigpond.net.au>
2016-04-25 19:31:49 +1000

    - Created a level with a plane and basic walls - Gave character basic translate functions to move about (rotation not working yet) - Gave character an energy system that effects its movespeed - Made camera follow (lookAt) the player

---
+++ Assets/followcam.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followcam : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LateUpdate()
+    {
+        float desAngle = player.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desAngle, 0);
+        transform.position = player.transform.position - (rotation * offset);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour {
+
+    public float moveSpeed = 10;
+    public float turnSpeed = 60;
+    public float energy = 100;
+
+    public Slider energySlider;
+
+	// Use this for initialization
+	void Start () {
+        energySlider.value = energy;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //update GUI
+        energySlider.value = energy;
+
+        //movement speed if energy available
+        if (energy > 0)
+        {
+            moveSpeed = 10;
+
+        } else
+        {
+            moveSpeed = 2;
+        }
+
+        //movement translations
+        float horizontal = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.Translate(horizontal, 0, 0);
+
+        float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical);
+
+        //lock rotation
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        //energy use
+        if (Input.GetAxis("Horizontal") != 0 )
+        {
+            energy -= 0.1f;
+        }
+
+        else if (Input.GetAxis("Vertical") != 0)
+        {
+            energy -= 0.1f;
+        }
+
+    }
+}
