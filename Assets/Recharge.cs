214782661b0344de2a7270d5f6bbcfffa3be6ffc
Patrick Freeman <n8890862@qut.edu.au>
2016-04-26 13:44:39 +1000

    - Imported Roomba Model - Created a charging port game object - Created a charging script that will charge the Roomba - Changed the roomba controls to tank controls

---
+++ Assets/Recharge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recharge : MonoBehaviour {
+
+	public bool charging;
+	private GameObject collision;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (charging) {
+            if (player.GetComponent<player>().energy < 100.0f)
+            {
+                player.GetComponent<player>().energy += 0.1f;
+            }
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		charging = true;
+	}
+
+	private void OnTriggerExit(Collider other) {
+		charging = false;
+	}
+}
